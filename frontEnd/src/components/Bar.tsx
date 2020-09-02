06ecff4aa612e5053b1fa84454fc40074ddd5e61
Ben Walker <ben.walker18@gmail.com>
2020-09-02 15:27:27 -0700

    Add state to bar on front end to display wallet info

--- frontEnd/src/components/Bar.tsx
+++ frontEnd/src/components/Bar.tsx
@@ -1,12 +1,13 @@
+import React, { ReactElement } from 'react';
+
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import { Grid } from '@material-ui/core';
-import React from 'react';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/styles';
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme: any) => ({
     root: {
         flexGrow: 1,
     },
@@ -16,34 +17,72 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
-}));
+});
 
-export function Bar() {
-    const classes = useStyles();
+const walletDisplay = <Button color="secondary" variant="contained">
+    walletDisplay
+</Button>
 
-    return (
-        <div className={classes.root}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Grid
-                        container
-                        direction="row"
-                        justify="space-around"
-                        alignItems="center"
-                    >
-                        <Grid>
-                            <Typography variant="h4" className={classes.title}>
-                                <img src={"favicon32x32.png"} alt="" /> Askolend
-                            </Typography>
-                        </Grid>
-                        <Grid>
-                            <Button color="secondary" variant="contained">
-                                Connect Wallet
-                            </Button>
-                        </Grid>
-                    </Grid>
-                </Toolbar>
-            </AppBar>
-        </div>
-    );
+interface IBarProps {
+    classes: any,
 }
+
+interface IBarState {
+    button: ReactElement,
+}
+
+class BarClass extends React.Component<IBarProps, IBarState>  {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            button: this.walletDisplay()
+        };
+    }
+
+    swapButtons = () => {
+        this.setState({ button: walletDisplay });
+    }
+
+    walletButton = () => {
+        return <Button color="secondary" variant="contained" onClick={() => this.setState({ button: this.walletDisplay() })}>
+            Connect Wallet
+        </Button>;
+    }
+
+    walletDisplay = () => {
+        return <Button color="secondary" variant="contained" onClick={() => this.setState({ button: this.walletButton() })}>
+            Wallet Info
+        </Button>;
+    }
+
+    render() {
+        const { classes } = this.props;
+        return (
+            <div className={classes.root}>
+                <AppBar position="static">
+                    <Toolbar>
+                        <Grid
+                            container
+                            direction="row"
+                            justify="space-around"
+                            alignItems="center"
+                        >
+                            <Grid>
+                                <Typography variant="h4" className={classes.title}>
+                                    <img src={"favicon32x32.png"} alt="" /> Askolend
+                            </Typography>
+                            </Grid>
+                            <Grid>
+                                {this.state.button}
+                            </Grid>
+                        </Grid>
+                    </Toolbar>
+                </AppBar>
+            </div>
+        );
+    }
+}
+
+
+var Bar: any = withStyles(styles)(BarClass);
+export { Bar };

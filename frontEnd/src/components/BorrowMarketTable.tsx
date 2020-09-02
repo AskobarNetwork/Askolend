3f58ab5f6e4ad0bf43a657f58d6dedd9635d86c5
Ben Walker <ben.walker18@gmail.com>
2020-09-02 15:32:12 -0700

    Fix token icons

--- frontEnd/src/components/BorrowMarketTable.tsx
+++ frontEnd/src/components/BorrowMarketTable.tsx
@@ -1,4 +1,4 @@
-import { Grid, Typography } from '@material-ui/core';
+import { Avatar, Grid, Typography } from '@material-ui/core';
 
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
@@ -22,13 +22,13 @@ function createData(icon: any, asset: string, apy: string, wallet: string, liqui
 }
 
 const rows = [
-    createData(<img src={"bat.png"} alt="" />, 'Basic Attention Token', '10.52%', '0 BAT', '$4.06M'),
-    createData(<img src={"dai.png"} alt="" />, 'Dai', '3.03%', '0 DAI', '$189.48M'),
-    createData(<img src={"favicon32x32.png"} alt="" />, 'Ether', '0.20%', '0 ETH', '$375.54M'),
-    createData(<img src={"usdc.png"} alt="" />, 'USD Coin', '1.89%', '0 USDC', '$127.10M'),
-    createData(<img src={"tether.png"} alt="" />, 'Tether', '2.75%', '0 USDT', '$4.63M'),
-    createData(<img src={"wbtc.png"} alt="" />, 'Wrapped BTC', '0.97%', '0 WBTC', '$13.11M'),
-    createData(<img src={"zrx.png"} alt="" />, '0x', '1.92%', '0 ZRX', '$52.67M'),
+    createData(<Avatar src={"bat.png"} alt="" />, 'Basic Attention Token', '10.52%', '0 BAT', '$4.06M'),
+    createData(<Avatar src={"dai.png"} alt="" />, 'Dai', '3.03%', '0 DAI', '$189.48M'),
+    createData(<Avatar src={"ether.png"} alt="" />, 'Ether', '0.20%', '0 ETH', '$375.54M'),
+    createData(<Avatar src={"usdc.png"} alt="" />, 'USD Coin', '1.89%', '0 USDC', '$127.10M'),
+    createData(<Avatar src={"tether.png"} alt="" />, 'Tether', '2.75%', '0 USDT', '$4.63M'),
+    createData(<Avatar src={"wbtc.png"} alt="" />, 'Wrapped BTC', '0.97%', '0 WBTC', '$13.11M'),
+    createData(<Avatar src={"zrx.png"} alt="" />, '0x', '1.92%', '0 ZRX', '$52.67M'),
 ];
 
 export function BorrowMarketTable() {
--- frontEnd/src/components/SupplyMarketTable.tsx
+++ frontEnd/src/components/SupplyMarketTable.tsx
@@ -1,4 +1,4 @@
-import { Grid, Switch, Typography } from '@material-ui/core';
+import { Avatar, Grid, Switch, Typography } from '@material-ui/core';
 
 import Paper from '@material-ui/core/Paper';
 import React from 'react';
@@ -22,13 +22,13 @@ function createData(icon: any, asset: string, apy: string, wallet: string, colla
 }
 
 const rows = [
-    createData(<img src={"bat.png"} alt="" />, 'Basic Attention Token', '10.52%', '0 BAT', false),
-    createData(<img src={"dai.png"} alt="" />, 'Dai', '3.03%', '0 DAI', false),
-    createData(<img src={"favicon32x32.png"} alt="" />, 'Ether', '0.20%', '0 ETH', false),
-    createData(<img src={"usdc.png"} alt="" />, 'USD Coin', '1.89%', '0 USDC', false),
-    createData(<img src={"tether.png"} alt="" />, 'Tether', '2.75%', '0 USDT', false),
-    createData(<img src={"wbtc.png"} alt="" />, 'Wrapped BTC', '0.97%', '0 WBTC', false),
-    createData(<img src={"zrx.png"} alt="" />, '0x', '1.92%', '0 ZRX', false),
+    createData(<Avatar src={"bat.png"} alt="" />, 'Basic Attention Token', '10.52%', '0 BAT', false),
+    createData(<Avatar src={"dai.png"} alt="" />, 'Dai', '3.03%', '0 DAI', false),
+    createData(<Avatar src={"ether.png"} alt="" />, 'Ether', '0.20%', '0 ETH', false),
+    createData(<Avatar src={"usdc.png"} alt="" />, 'USD Coin', '1.89%', '0 USDC', false),
+    createData(<Avatar src={"tether.png"} alt="" />, 'Tether', '2.75%', '0 USDT', false),
+    createData(<Avatar src={"wbtc.png"} alt="" />, 'Wrapped BTC', '0.97%', '0 WBTC', false),
+    createData(<Avatar src={"zrx.png"} alt="" />, '0x', '1.92%', '0 ZRX', false),
 ];
 
 export function SupplyMarketTable() {
